be34dcd09eb5f269fefe76cf2574df44cd1a89a6
AlekseyMegliy <meglij46@gmail.com>
2023-01-18 20:38:35 +0200

    sudoku is done

--- sudoku.js
+++ sudoku.js
@@ -1,30 +1,20 @@
-var cells = document.getElementsByTagName('input');
+var cells = document.getElementsByClassName('cell');
+var space_num = document.getElementById('space_num');
+var start_but = document.querySelector('.start');
 var cells_mas = [];
 var member_mas_row = [[], [], [], [], [], [], [], [], []];
 var member_mas_col = [[], [], [], [], [], [], [], [], []];
-// let minitab_mas:number[][] = [];
+var check_mas = [[], [], [], [], [], [], [], [], []];
 for (var _i = 0, cells_1 = cells; _i < cells_1.length; _i++) {
     var cell = cells_1[_i];
     cells_mas.push(cell);
 }
-function clearing() {
-    for (var j = 0; j <= 8; j++) {
-        for (var i = 0; i <= 8; i++) {
-            member_mas_row[j][i] = 0;
-            member_mas_col[i][j] = 0;
-            cells_mas[j * 9 + i].value = '';
-            console.log(i);
-        }
-    }
-}
-var done = false;
 function start() {
     for (var j = 0; j <= 8; j++) {
         for (var i = 0; i <= 8; i++) {
             member_mas_row[j][i] = 0;
             member_mas_col[i][j] = 0;
             cells_mas[j * 9 + i].value = '';
-            console.log(i);
         }
     }
     for (var j = 0; j <= 8; j++) {
@@ -33,24 +23,24 @@ function start() {
                 if (i + 1 + j * 3 + 2 > 9) {
                     if (i + 1 + j * 3 + 2 - 9 > 9) {
                         if (i + 1 + j * 3 + 2 - 9 > 9 * 2) {
-                            cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 2 - 9 * 3);
+                            // cells_mas[j*9+i].value = `${i+1+j*3+2-9*3}`
                             member_mas_row[j][i] = i + 1 + j * 3 + 2 - 9 * 3;
                             member_mas_col[i][j] = i + 1 + j * 3 + 2 - 9 * 3;
                         }
                         else {
-                            cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 2 - 9 * 2);
+                            // cells_mas[j*9+i].value = `${i+1+j*3+2-9*2}`
                             member_mas_row[j][i] = i + 1 + j * 3 + 2 - 9 * 2;
                             member_mas_col[i][j] = i + 1 + j * 3 + 2 - 9 * 2;
                         }
                     }
                     else {
-                        cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 2 - 9);
+                        // cells_mas[j*9+i].value = `${i+1+j*3+2-9}`
                         member_mas_row[j][i] = i + 1 + j * 3 + 2 - 9;
                         member_mas_col[i][j] = i + 1 + j * 3 + 2 - 9;
                     }
                 }
                 else {
-                    cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 2);
+                    // cells_mas[j*9+i].value = `${i+1+j*3+2}`
                     member_mas_row[j][i] = i + 1 + j * 3 + 2;
                     member_mas_col[i][j] = i + 1 + j * 3 + 2;
                 }
@@ -59,24 +49,24 @@ function start() {
                 if (i + 1 + j * 3 + 1 > 9) {
                     if (i + 1 + j * 3 + 1 - 9 > 9) {
                         if (i + 1 + j * 3 + 1 - 9 > 9 * 2) {
-                            cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 1 - 9 * 3);
+                            // cells_mas[j*9+i].value = `${i+1+j*3+1-9*3}`
                             member_mas_row[j][i] = i + 1 + j * 3 + 1 - 9 * 3;
                             member_mas_col[i][j] = i + 1 + j * 3 + 1 - 9 * 3;
                         }
                         else {
-                            cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 1 - 9 * 2);
+                            // cells_mas[j*9+i].value = `${i+1+j*3+1-9*2}`
                             member_mas_row[j][i] = i + 1 + j * 3 + 1 - 9 * 2;
                             member_mas_col[i][j] = i + 1 + j * 3 + 1 - 9 * 2;
                         }
                     }
                     else {
-                        cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 1 - 9);
+                        // cells_mas[j*9+i].value = `${i+1+j*3+1-9}`
                         member_mas_row[j][i] = i + 1 + j * 3 + 1 - 9;
                         member_mas_col[i][j] = i + 1 + j * 3 + 1 - 9;
                     }
                 }
                 else {
-                    cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 + 1);
+                    // cells_mas[j*9+i].value = `${i+1+j*3+1}`
                     member_mas_row[j][i] = i + 1 + j * 3 + 1;
                     member_mas_col[i][j] = i + 1 + j * 3 + 1;
                 }
@@ -85,31 +75,137 @@ function start() {
                 if (i + 1 + j * 3 > 9) {
                     if (i + 1 + j * 3 - 9 > 9) {
                         if (i + 1 + j * 3 - 9 > 9 * 2) {
-                            cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 - 9 * 3);
+                            // cells_mas[j*9+i].value = `${i+1+j*3-9*3}`
                             member_mas_row[j][i] = i + 1 + j * 3 - 9 * 3;
                             member_mas_col[i][j] = i + 1 + j * 3 - 9 * 3;
                         }
                         else {
-                            cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 - 9 * 2);
+                            // cells_mas[j*9+i].value = `${i+1+j*3-9*2}`
                             member_mas_row[j][i] = i + 1 + j * 3 - 9 * 2;
                             member_mas_col[i][j] = i + 1 + j * 3 - 9 * 2;
                         }
                     }
                     else {
-                        cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3 - 9);
+                        // cells_mas[j*9+i].value = `${i+1+j*3-9}`
                         member_mas_row[j][i] = i + 1 + j * 3 - 9;
                         member_mas_col[i][j] = i + 1 + j * 3 - 9;
                     }
                 }
                 else {
-                    cells_mas[j * 9 + i].value = "".concat(i + 1 + j * 3);
+                    // cells_mas[j*9+i].value = `${i+1+j*3}`
                     member_mas_row[j][i] = i + 1 + j * 3;
                     member_mas_col[i][j] = i + 1 + j * 3;
                 }
             }
         }
     }
+    var random_line = 1;
+    var swap_line;
+    function random_row() {
+        random_line = Math.floor(Math.random() * 9);
+        if ((random_line + 1) % 3 === 0) {
+            var random_line_swap = Math.round(Math.random() + 1);
+            swap_line = member_mas_row[random_line];
+            member_mas_row[random_line] = member_mas_row[random_line - random_line_swap];
+            member_mas_row[random_line - random_line_swap] = swap_line;
+        }
+        else if ((random_line + 1) % 3 === 2) {
+            var random_line_swap = Math.pow((-1), Math.round(Math.random() + 1));
+            swap_line = member_mas_row[random_line];
+            member_mas_row[random_line] = member_mas_row[random_line - random_line_swap];
+            member_mas_row[random_line - random_line_swap] = swap_line;
+        }
+        else if ((random_line + 1) % 3 === 1) {
+            var random_line_swap = Math.round(Math.random());
+            swap_line = member_mas_row[random_line];
+            member_mas_row[random_line] = member_mas_row[random_line + random_line_swap];
+            member_mas_row[random_line + random_line_swap] = swap_line;
+        }
+        for (var j = 0; j <= 8; j++) {
+            for (var i = 0; i <= 8; i++) {
+                member_mas_col[i][j] = member_mas_row[j][i];
+            }
+        }
+    }
+    function random_col() {
+        random_line = Math.floor(Math.random() * 9);
+        if ((random_line + 1) % 3 === 0) {
+            var random_line_swap = Math.round(Math.random() + 1);
+            swap_line = member_mas_col[random_line];
+            member_mas_col[random_line] = member_mas_col[random_line - random_line_swap];
+            member_mas_col[random_line - random_line_swap] = swap_line;
+        }
+        else if ((random_line + 1) % 3 === 2) {
+            var random_line_swap = Math.pow((-1), Math.round(Math.random() + 1));
+            swap_line = member_mas_col[random_line];
+            member_mas_col[random_line] = member_mas_col[random_line - random_line_swap];
+            member_mas_col[random_line - random_line_swap] = swap_line;
+        }
+        else if ((random_line + 1) % 3 === 1) {
+            var random_line_swap = Math.round(Math.random());
+            swap_line = member_mas_col[random_line];
+            member_mas_col[random_line] = member_mas_col[random_line + random_line_swap];
+            member_mas_col[random_line + random_line_swap] = swap_line;
+        }
+        for (var j = 0; j <= 8; j++) {
+            for (var i = 0; i <= 8; i++) {
+                member_mas_row[j][i] = member_mas_col[i][j];
+            }
+        }
+    }
+    for (var i = 0; i < 100; i++) {
+        random_col();
+        random_row();
+    }
+    for (var j = 0; j <= 8; j++) {
+        for (var i = 0; i <= 8; i++) {
+            check_mas[i][j] = member_mas_row[j][i];
+        }
+    }
+    var new_col_mas;
+    new_col_mas = member_mas_col;
+    function space(num) {
+        for (var i = 0; i < num; i++) {
+            var random_rowcell = Math.floor(Math.random() * 8);
+            var random_colcell = Math.floor(Math.random() * 8);
+            new_col_mas[random_rowcell][random_colcell] = 0;
+        }
+    }
+    space(Number(space_num.value));
+    for (var j = 0; j <= 8; j++) {
+        for (var i = 0; i <= 8; i++) {
+            if (new_col_mas[j][i] === 0) {
+                cells_mas[j * 9 + i].value = "";
+            }
+            else {
+                cells_mas[j * 9 + i].value = "".concat(new_col_mas[j][i]);
+            }
+        }
+    }
 }
 function check() {
-    alert(member_mas_row);
+    var wrong_empty = 0;
+    for (var _i = 0, cells_2 = cells; _i < cells_2.length; _i++) {
+        var cell = cells_2[_i];
+        cell.toggleAttribute("readonly");
+    }
+    start_but.toggleAttribute("disabled");
+    for (var j = 0; j <= 8; j++) {
+        for (var i = 0; i <= 8; i++) {
+            if (Number(cells_mas[j * 9 + i].value) === 0) {
+                cells_mas[j * 9 + i].classList.toggle("empty");
+                wrong_empty++;
+            }
+            else if (Number(cells_mas[j * 9 + i].value) === check_mas[j][i]) {
+                cells_mas[j * 9 + i].classList.toggle("right");
+            }
+            else if (Number(cells_mas[j * 9 + i].value) != check_mas[j][i]) {
+                cells_mas[j * 9 + i].classList.toggle("wrong");
+                wrong_empty++;
+            }
+        }
+    }
+    if (wrong_empty === 0) {
+        alert("Victory!!!!");
+    }
 }
--- sudoku.ts
+++ sudoku.ts
@@ -1,25 +1,16 @@
-let cells = document.getElementsByTagName('input');
-let cells_mas:Array<any> = [];
+let cells:HTMLCollectionOf<HTMLInputElement> = document.getElementsByClassName('cell');
+let space_num:HTMLInputElement = document.getElementById('space_num')
+let start_but:HTMLButtonElement = document.querySelector('.start')
+let cells_mas:Array<HTMLInputElement> = [];
 let member_mas_row:number[][] = [[],[],[],[],[],[],[],[],[]];
 let member_mas_col:number[][] = [[],[],[],[],[],[],[],[],[]];
-// let minitab_mas:number[][] = [];
 
+let check_mas:number[][]=[[],[],[],[],[],[],[],[],[]];
 
 for(let cell of cells){
     cells_mas.push(cell);
 }
-function clearing(){
 
-    for(let j=0; j<=8; j++){
-        for(let i=0; i<=  8; i++){
-            member_mas_row[j][i] =0
-            member_mas_col[i][j] =0
-            cells_mas[j*9+i].value='';
-            console.log(i)
-        }
-    }
-}
-let done = false
 function start(){
     
     
@@ -28,7 +19,7 @@ function start(){
                 member_mas_row[j][i] =0
                 member_mas_col[i][j] =0
                 cells_mas[j*9+i].value='';
-                console.log(i)
+  
             }
         }
         for(let j=0; j<=8; j++){
@@ -38,26 +29,26 @@ function start(){
                     if(i+1+j*3+2>9){
                         if(i+1+j*3+2-9 > 9){
                             if(i+1+j*3+2-9 > 9*2){
-                                cells_mas[j*9+i].value = `${i+1+j*3+2-9*3}`
+                                // cells_mas[j*9+i].value = `${i+1+j*3+2-9*3}`
                                 member_mas_row[j][i] = i+1+j*3+2-9*3
                                 member_mas_col[i][j] = i+1+j*3+2-9*3
                             }
                             else{
-                                cells_mas[j*9+i].value = `${i+1+j*3+2-9*2}`
+                                // cells_mas[j*9+i].value = `${i+1+j*3+2-9*2}`
                                 member_mas_row[j][i] = i+1+j*3+2-9*2
                                 member_mas_col[i][j] = i+1+j*3+2-9*2
                             }
                                 
                         }
                         else{
-                            cells_mas[j*9+i].value = `${i+1+j*3+2-9}`
+                            // cells_mas[j*9+i].value = `${i+1+j*3+2-9}`
                             member_mas_row[j][i] = i+1+j*3+2-9
                             member_mas_col[i][j] = i+1+j*3+2-9
                         }
                         
                     }
                     else{
-                        cells_mas[j*9+i].value = `${i+1+j*3+2}`
+                        // cells_mas[j*9+i].value = `${i+1+j*3+2}`
                         member_mas_row[j][i] = i+1+j*3+2
                         member_mas_col[i][j] = i+1+j*3+2
                     }
@@ -69,26 +60,26 @@ function start(){
                     if(i+1+j*3+1>9){
                         if(i+1+j*3+1-9 > 9){
                             if(i+1+j*3+1-9 > 9*2){
-                                cells_mas[j*9+i].value = `${i+1+j*3+1-9*3}`
+                                // cells_mas[j*9+i].value = `${i+1+j*3+1-9*3}`
                                 member_mas_row[j][i] = i+1+j*3+1-9*3
                                 member_mas_col[i][j] = i+1+j*3+1-9*3
                             }
                             else{
-                                cells_mas[j*9+i].value = `${i+1+j*3+1-9*2}`
+                                // cells_mas[j*9+i].value = `${i+1+j*3+1-9*2}`
                                 member_mas_row[j][i] = i+1+j*3+1-9*2
                                 member_mas_col[i][j] = i+1+j*3+1-9*2
                             }
                                 
                         }
                         else{
-                            cells_mas[j*9+i].value = `${i+1+j*3+1-9}`
+                            // cells_mas[j*9+i].value = `${i+1+j*3+1-9}`
                             member_mas_row[j][i] = i+1+j*3+1-9
                             member_mas_col[i][j] = i+1+j*3+1-9
                         }
                         
                     }
                     else{
-                        cells_mas[j*9+i].value = `${i+1+j*3+1}`
+                        // cells_mas[j*9+i].value = `${i+1+j*3+1}`
                         member_mas_row[j][i] = i+1+j*3+1
                         member_mas_col[i][j] = i+1+j*3+1
                     }
@@ -100,40 +91,158 @@ function start(){
                     if(i+1+j*3>9){
                         if(i+1+j*3-9 > 9){
                             if(i+1+j*3-9 > 9*2){
-                                cells_mas[j*9+i].value = `${i+1+j*3-9*3}`
+                                // cells_mas[j*9+i].value = `${i+1+j*3-9*3}`
                                 member_mas_row[j][i] = i+1+j*3-9*3
                                 member_mas_col[i][j] = i+1+j*3-9*3
                             }
                             else{
-                                cells_mas[j*9+i].value = `${i+1+j*3-9*2}`
+                                // cells_mas[j*9+i].value = `${i+1+j*3-9*2}`
                                 member_mas_row[j][i] = i+1+j*3-9*2
                                 member_mas_col[i][j] = i+1+j*3-9*2
                             }
                                 
                         }
                         else{
-                            cells_mas[j*9+i].value = `${i+1+j*3-9}`
+                            // cells_mas[j*9+i].value = `${i+1+j*3-9}`
                             member_mas_row[j][i] = i+1+j*3-9
                             member_mas_col[i][j] = i+1+j*3-9
                         }
                         
                     }
                     else{
-                        cells_mas[j*9+i].value = `${i+1+j*3}`
+                        // cells_mas[j*9+i].value = `${i+1+j*3}`
                         member_mas_row[j][i] = i+1+j*3
                         member_mas_col[i][j] = i+1+j*3
                     }
 
                 }
-                
-                
-                
-           
         }
     
     }
+    let random_line:number=1;
+    let swap_line:number[];
+
+    function random_row(){
+        random_line = Math.floor(Math.random()*9)
+        if((random_line+1)%3===0){
+            let random_line_swap:number= Math.round(Math.random()+1)
+            swap_line = member_mas_row[random_line];
+            member_mas_row[random_line] = member_mas_row[random_line-random_line_swap]
+            member_mas_row[random_line-random_line_swap]=swap_line  
+        }
+        else if((random_line+1)%3===2){
+            let random_line_swap:number= Math.pow((-1),Math.round(Math.random()+1) )
+            swap_line = member_mas_row[random_line];
+            member_mas_row[random_line] = member_mas_row[random_line-random_line_swap]
+            member_mas_row[random_line-random_line_swap]=swap_line  
+        }
+        else if((random_line+1)%3===1){
+            let random_line_swap:number= Math.round(Math.random())
+            swap_line = member_mas_row[random_line];
+            member_mas_row[random_line] = member_mas_row[random_line+random_line_swap]
+            member_mas_row[random_line+random_line_swap]=swap_line  
+        }
+        for(let j=0; j<=8; j++){
+            for(let i=0; i<=  8; i++){
+                member_mas_col[i][j]=member_mas_row[j][i] ;
+            }
+        }
+    }
+
+    function random_col(){
+        random_line = Math.floor(Math.random()*9)
+        if((random_line+1)%3===0){
+            let random_line_swap:number= Math.round(Math.random()+1)
+            swap_line = member_mas_col[random_line];
+            member_mas_col[random_line] = member_mas_col[random_line - random_line_swap]
+            member_mas_col[random_line - random_line_swap]=swap_line  
+        }
+        else if((random_line+1)%3===2){
+            let random_line_swap:number= Math.pow((-1),Math.round(Math.random()+1) )
+            swap_line = member_mas_col[random_line];
+            member_mas_col[random_line] = member_mas_col[random_line - random_line_swap]
+            member_mas_col[random_line - random_line_swap]=swap_line  
+        }
+        else if((random_line+1)%3===1){
+            let random_line_swap:number= Math.round(Math.random())
+            swap_line = member_mas_col[random_line];
+            member_mas_col[random_line] = member_mas_col[random_line + random_line_swap]
+            member_mas_col[random_line + random_line_swap]=swap_line  
+        }
+        for(let j=0; j<=8; j++){
+            for(let i=0; i<=  8; i++){
+                member_mas_row[j][i]=member_mas_col[i][j] ;
+            }
+        }
+    }
+
+    for(let i=0; i<100;i++){
+        random_col();
+        random_row();
+        
+        
+    }
+
+    for(let j=0; j<=8; j++){
+        for(let i=0; i<=  8; i++){
+            check_mas[i][j]=member_mas_row[j][i] ;
+        }
+    }
+    let new_col_mas:number[][];
+    new_col_mas = member_mas_col
+   
+   
+
+    function space(num:number){
+        for(let i=0; i<num; i++){
+            let random_rowcell:number = Math.floor(Math.random()*8)
+            let random_colcell:number = Math.floor(Math.random()*8)
+            new_col_mas[random_rowcell][random_colcell] = 0
+
+        }
+    }
+    space(Number(space_num.value));
+    
+    for(let j=0; j<=8; j++){
+        for(let i=0; i<=  8; i++){
+            if(new_col_mas[j][i]===0){
+                cells_mas[j*9+i].value=""
+            }else{ 
+                cells_mas[j*9+i].value=`${new_col_mas[j][i] }`;
+            }
+           
+        }
+    }
+    
+    
 }  
 
 function check(){
-    alert(member_mas_row)
+    let wrong_empty:number=0
+    for(let cell of cells){
+        cell.toggleAttribute("readonly");
+    }
+    start_but.toggleAttribute("disabled")
+    for(let j=0; j<=8; j++){
+        for(let i=0; i<=  8; i++){
+            if(Number(cells_mas[j*9+i].value)===0){
+                cells_mas[j*9+i].classList.toggle("empty")
+                wrong_empty++
+            }
+            else if(Number(cells_mas[j*9+i].value)===check_mas[j][i]){
+                cells_mas[j*9+i].classList.toggle("right")
+            } 
+            else if(Number(cells_mas[j*9+i].value)!=check_mas[j][i]){
+                cells_mas[j*9+i].classList.toggle("wrong")
+                wrong_empty++
+
+            }
+            
+
+        }
+    }
+    if(wrong_empty===0){
+        alert("Victory!!!!")
+    }
+  
 }
